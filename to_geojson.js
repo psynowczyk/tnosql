b66999b80ec9b8e6fcfa8651a59dad3733e8fbf1
Piotr Synowczyk <piotrsynowczyk@wp.pl>
2014-11-16 20:53:34 +0100

    added 1d.2 and 1d.3

--- to_geojson.js
+++ to_geojson.js
@@ -1,8 +1,44 @@
 var connection = new Mongo();
 var db = connection.getDB('Trains');
 
+/*
+// 1d.1
 var gdansk = { "type": "Point", "coordinates": [18.65, 54.35] }
-var array = db.fuel.find({ loc: { $near: { $geometry: gdansk }, $maxDistance: 20000 } }).limit(10).toArray();
+var array = db.fuel.find({
+	loc: {
+		$near: {
+			$geometry: gdansk
+		},
+		$maxDistance: 20000
+	}
+}).limit(10).toArray();
+
+// 1d.2
+var olsztyn = { "type": "Point", "coordinates": [20.48, 53.78] }
+var array = db.fuel.find({
+	loc: {
+		$geoWithin: {
+			$center: [[olsztyn.coordinates[0], olsztyn.coordinates[1]], 0.80]
+		}
+	}
+}).limit(5).toArray();
+*/
+var array = db.fuel.find({
+	loc: {
+		$geoWithin: {
+			$geometry: {
+				"type": "Polygon",
+				"coordinates": [[
+					[18.65, 54.35],
+					[20.48, 53.78],
+					[16.93, 52.41],
+					[18.65, 54.35]
+				]]
+			}
+		}
+	}
+}).limit(100).toArray();
+
 var geojson = {
 	"type": "FeatureCollection",
 	"features": []
